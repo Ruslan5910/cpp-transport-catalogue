abc5b14f25a158fa33f037f955d0f6209ffb1d6a
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2024-10-07 21:39:40 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,40 +1,44 @@
 #include "stat_reader.h"
+#include <string_view>
 
 namespace transport {
     
-void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output) {
-    TransportCatalogue::RouteInfo route_info;
-    auto space = request.find(' ');
-    auto last_char = request.find_last_not_of(' ');
-    std::string line = std::string(request.substr(space + 1, last_char + 1 - space));
-    std::string command = std::string(request.substr(0, space));
-    if (command == "Bus") {
-        route_info = transport_catalogue.GetRouteInfo(line);
-        if (route_info.count_stops == 0) {
-            output << std::string("Bus ") << line << std::string(": not found") << std::endl;
-        } else {
-            output << std::string("Bus ") << line << std::string(": ") << route_info.count_stops << std::string(" stops on route, ") 
-               << route_info.count_unique_stops << std::string(" unique stops, ") << route_info.route_length << std::string(" route length") << std::endl;
-        }
-    } else if (command == "Stop") {
-         if (transport_catalogue.FindStopByName(line) == nullptr) {
-            output << std::string("Stop ") << line << std::string(": not found") << std::endl;
-             return;
-        }
-        const auto result = std::set<std::string_view>(transport_catalogue.GetRoutesThrowStop(line).begin(), transport_catalogue.GetRoutesThrowStop(line).end());
-        if (result.empty()) {
-            output << std::string("Stop ") << line << std::string(": no buses") << std::endl;
-        } else {
-            output << std::string("Stop ") << line << std::string(": buses");
-            for (const std::string_view& route : result) {
-                output << std::string(" ") << route;
+void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::ostream& output, std::istream& input) {
+    int stat_request_count;
+    input >> stat_request_count >> std::ws;
+    for (int i = 0; i < stat_request_count; ++i) {
+       std::string request;
+       std::getline(input, request);     
+       TransportCatalogue::RouteInfo route_info;
+       auto space = request.find(' ');
+        auto last_char = request.find_last_not_of(' ');
+        std::string line = std::string(request.substr(space + 1, last_char + 1 - space));
+        std::string command = std::string(request.substr(0, space));
+        if (command == "Bus") {
+            route_info = transport_catalogue.GetRouteInfo(line);
+            if (route_info.count_stops == 0) {
+                output << std::string("Bus ") << line << std::string(": not found") << std::endl;
+            } else {
+                output << std::string("Bus ") << line << std::string(": ") << route_info.count_stops << std::string(" stops on route, ") 
+                << route_info.count_unique_stops << std::string(" unique stops, ") << route_info.route_length << std::string(" route length") << std::endl;
             }
-            output << std::endl;
+        } else if (command == "Stop") {
+            auto result = transport_catalogue.GetRoutesThrowStop(line);        
+            if (!result) {
+                output << std::string("Stop ") << line << std::string(": not found") << std::endl;
+            } else if (result->empty()) {
+                output << std::string("Stop ") << line << std::string(": no buses") << std::endl;
+            } else {
+                output << std::string("Stop ") << line << std::string(": buses");
+                for (const std::string_view& route : std::set<std::string_view>(result->begin(), result->end())) {
+                    output << std::string(" ") << route;
+                }
+                output << std::endl;
+            }
+        } else {
+            throw std::invalid_argument("unknown command");
         }
-    } else {
-        throw std::invalid_argument("unknown command");
     }
 }
-
     
 } // namespace transport
