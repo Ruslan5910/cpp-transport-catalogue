23ce28eca7ce64370b1004a13bd51dd43ac19b50
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2024-10-07 21:37:06 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -9,25 +9,9 @@ using namespace transport;
 
 int main() {
     TransportCatalogue catalogue;
-
-    int base_request_count;
-    cin >> base_request_count >> ws;
-
-    {
-        InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
-            string line;
-            getline(cin, line);
-            reader.ParseLine(line);
-        }
+    {    
+        InputReader reader(cin);
         reader.ApplyCommands(catalogue);
     }
-
-    int stat_request_count;
-    cin >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
-        string line;
-        getline(cin, line);
-        ParseAndPrintStat(catalogue, line, cout);
-    }
+    ParseAndPrintStat(catalogue, cout, cin);
 }
