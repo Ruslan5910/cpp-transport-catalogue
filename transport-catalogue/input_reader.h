84c9b102bab2cf3462a54db8ad5eb3ab4e582ee5
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2024-10-07 17:30:03 +0300

    input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,43 @@
-// место для вашего кода
+#pragma once
+
+#include "geo.h"
+#include "transport_catalogue.h"
+
+namespace transport {
+    
+namespace parsewords {
+    
+struct CommandDescription {
+    // Определяет, задана ли команда (поле command непустое)
+    explicit operator bool() const {
+        return !command.empty();
+    }
+
+    bool operator!() const {
+        return !operator bool();
+    }
+
+    std::string command;      // Название команды
+    std::string id;           // id маршрута или остановки
+    std::string description;  // Параметры команды
+};
+
+} // namespace parsewords
+    
+class InputReader {
+public:
+    /**
+     * Парсит строку в структуру CommandDescription и сохраняет результат в commands_
+     */
+    void ParseLine(std::string_view line);
+
+    /**
+     * Наполняет данными транспортный справочник, используя команды из commands_
+     */
+    void ApplyCommands(TransportCatalogue& catalogue);
+
+private:
+    std::vector<parsewords::CommandDescription> commands_;
+};
+    
+} // namespace transport
