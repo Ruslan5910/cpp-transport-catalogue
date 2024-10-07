704dbdc240569c7e04bac7cf7de064926ba90b60
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2024-10-07 17:30:40 +0300

    stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,13 @@
-// место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+#include <iostream>
+
+#include "transport_catalogue.h"
+
+namespace transport {
+
+void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request, std::ostream& output);
+    
+} // namespace transport
