1fa4e8d0bbfb5a1afbc04ebfa5a1ed110e7d9b93
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2024-10-26 16:46:53 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -27,7 +27,16 @@ public:
     struct RouteInfo {
         size_t count_stops = 0;
         size_t count_unique_stops = 0;
-        double route_length = 0.;
+        int route_length = 0;
+        double straight_line_distance = 0.;
+    };
+    
+    struct Hasher {
+        size_t operator()(const std::pair<const Stop*, const Stop*>& stops) const {
+            std::hash<std::string> s_hasher;
+            size_t s1 = (s_hasher(stops.first->stop_name) * 37) + s_hasher(stops.second->stop_name);
+            return s1;
+        }
     };
     
     // добавление маршрута в базу
@@ -35,6 +44,9 @@ public:
     
     // добавление остановки в базу
     void AddStopAndCoordinates(const std::string& stop, const geo::Coordinates& coordinates);
+    
+    // добавление дистнции между остановками
+    void AddDistance(std::string_view, std::string_view, int distance);
     
     // поиск маршрута по имени
     const Bus* FindRouteByName(std::string_view route_name) const;
@@ -45,6 +57,9 @@ public:
     // получение информации о маршруте
     RouteInfo GetRouteInfo(std::string_view route_name) const;
     
+    // получение дистанции между остановками
+    int GetDistance(std::string_view from, std::string_view to) const;
+    
     // получить маршруты проходящие через остановку
     const std::unordered_set<std::string_view>* GetRoutesThrowStop(std::string_view stop_name) const;
     
@@ -54,6 +69,7 @@ private:
     std::unordered_map<std::string_view, const Stop*> stop_info_by_stop_name_;
     std::unordered_map<std::string_view, const Bus*> route_info_by_route_name_;
     std::unordered_map<std::string_view, std::unordered_set<std::string_view>> routes_throw_stop_;
+    std::unordered_map<std::pair<const Stop*, const Stop*>, int, Hasher> distance_between_stops_;
 };
 
 } // namespace transport
