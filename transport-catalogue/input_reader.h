95ea078f7f5b75ec11369760cc7e08fdab05600d
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2024-10-26 16:44:32 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -3,11 +3,26 @@
 #include <iostream>
 
 #include "geo.h"
+#pragma once
+
+#include <iostream>
+
+#include "geo.h"
 #include "transport_catalogue.h"
 
 namespace transport {
     
 namespace parsewords {
+    
+struct StopsAndMeters {
+    std::vector<std::string_view> stops_to;
+    std::vector<int> meters;
+};   
+    
+struct ParseDescription {
+    std::string_view coordinates;
+    std::string_view distances;
+};
     
 struct CommandDescription {
     // Определяет, задана ли команда (поле command непустое)
@@ -35,7 +50,7 @@ public:
         in_ >> base_request_count >> std::ws;
         for (int i = 0; i < base_request_count; ++i) {
             std::string line;
-            std::getline(in_, line);
+            std::getline(in_, line);   //Stop Marushkino: 55.595884, 37.209755, 9900m to Rasskazovka, 100m to Marushkino
             ParseLine(line);
         }
     }      
