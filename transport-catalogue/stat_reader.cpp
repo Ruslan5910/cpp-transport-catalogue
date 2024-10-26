85a4bcbafc0a4bc8a2f130a089fc1b90f6a65f6d
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2024-10-26 16:45:28 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -7,10 +7,10 @@ void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::ostre
     int stat_request_count;
     input >> stat_request_count >> std::ws;
     for (int i = 0; i < stat_request_count; ++i) {
-       std::string request;
-       std::getline(input, request);     
-       TransportCatalogue::RouteInfo route_info;
-       auto space = request.find(' ');
+        std::string request;
+        std::getline(input, request);     
+        TransportCatalogue::RouteInfo route_info;
+        auto space = request.find(' ');
         auto last_char = request.find_last_not_of(' ');
         std::string line = std::string(request.substr(space + 1, last_char + 1 - space));
         std::string command = std::string(request.substr(0, space));
@@ -20,7 +20,7 @@ void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::ostre
                 output << std::string("Bus ") << line << std::string(": not found") << std::endl;
             } else {
                 output << std::string("Bus ") << line << std::string(": ") << route_info.count_stops << std::string(" stops on route, ") 
-                << route_info.count_unique_stops << std::string(" unique stops, ") << route_info.route_length << std::string(" route length") << std::endl;
+                << route_info.count_unique_stops << std::string(" unique stops, ") << route_info.route_length << std::string(" route length, ") << route_info.route_length/route_info.straight_line_distance << " curvature"<<std::endl;
             }
         } else if (command == "Stop") {
             auto result = transport_catalogue.GetRoutesThrowStop(line);        
