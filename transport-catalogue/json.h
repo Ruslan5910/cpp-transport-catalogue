2b84f642371a696308f3ef9448611310df0b980a
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2024-12-03 19:55:59 +0300

    Update json.h

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -3,78 +3,160 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <vector>
 #include <variant>
+#include <vector>
 
 namespace json {
 
 class Node;
-
 using Dict = std::map<std::string, Node>;
 using Array = std::vector<Node>;
 
-// Эта ошибка должна выбрасываться при ошибках парсинга JSON
 class ParsingError : public std::runtime_error {
 public:
     using runtime_error::runtime_error;
 };
 
-class Node {
-public:
-    using Value = std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>;
+class Node final : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> { 
+public: 
     
-    Node() = default;
-    Node(std::nullptr_t);
-    Node(Array);
-    Node(Dict);
-    Node(bool);
-    Node(int);
-    Node(double);
-    Node(std::string);
+    using variant::variant;
+    using Value = variant;
+        
+    bool IsInt() const {
+        return std::holds_alternative<int>(*this);
+    }
+    int AsInt() const {
+        using namespace std::literals;
+        if (!IsInt()) {
+            throw std::logic_error("Not an int"s);
+        }
+        return std::get<int>(*this);
+    }
+
+    bool IsPureDouble() const {
+        return std::holds_alternative<double>(*this);
+    }
+    bool IsDouble() const {
+        return IsInt() || IsPureDouble();
+    }
+    double AsDouble() const {
+        using namespace std::literals;
+        if (!IsDouble()) {
+            throw std::logic_error("Not a double"s);
+        }
+        return IsPureDouble() ? std::get<double>(*this) : AsInt();
+    }
+
+    bool IsBool() const {
+        return std::holds_alternative<bool>(*this);
+    }
+    bool AsBool() const {
+        using namespace std::literals;
+        if (!IsBool()) {
+            throw std::logic_error("Not a bool"s);
+        }
+
+        return std::get<bool>(*this);
+    }
+
+    bool IsNull() const {
+        return std::holds_alternative<std::nullptr_t>(*this);
+    }
+
+    bool IsArray() const {
+        return std::holds_alternative<Array>(*this);
+    }
     
-    const Value& GetValue() const { return value_; }
+    const Array& AsArray() const {
+        using namespace std::literals;
+        if (!IsArray()) {
+            throw std::logic_error("Not an array"s);
+        }
+
+        return std::get<Array>(*this);
+    }
     
-    bool IsNull() const;
-    bool IsArray() const;
-    bool IsMap() const;
-    bool IsBool() const;
-    bool IsInt() const;
-    bool IsDouble() const;
-    bool IsPureDouble() const;
-    bool IsString() const;
+    Array& AsArray() {
+        using namespace std::literals;
+        if (!IsArray()) {
+            throw std::logic_error("Not an array"s);
+        }
+        return std::get<Array>(*this);
+    }
+
+    bool IsString() const {
+        return std::holds_alternative<std::string>(*this);
+    }
+    const std::string& AsString() const {
+        using namespace std::literals;
+        if (!IsString()) {
+            throw std::logic_error("Not a string"s);
+        }
+
+        return std::get<std::string>(*this);
+    }
+
+    bool IsDict() const {
+        return std::holds_alternative<Dict>(*this);
+    }
+    const Dict& AsDict() const {
+        using namespace std::literals;
+        if (!IsDict()) {
+            throw std::logic_error("Not a dict"s);
+        }
+
+        return std::get<Dict>(*this);
+    }
+    Dict& AsDict() {
+        using namespace std::literals;
+        if (!IsDict()) {
+            throw std::logic_error("Not a dict"s);
+        }
+
+        return std::get<Dict>(*this);
+    }    
+
+    bool operator==(const Node& rhs) const {
+        return GetValue() == rhs.GetValue();
+    }
+
+    const Value& GetValue() const {
+        return *this;
+    }
     
-    //Ниже перечислены методы, которые возвращают хранящееся внутри Node значение заданного типа.
-    //Если внутри содержится значение другого типа, должно выбрасываться исключение std::logic_error.
-    int AsInt() const;
-    bool AsBool() const;
-    // Возвращает значение типа double, если внутри хранится double либо int.
-    // В последнем случае возвращается приведенное в double значение.
-    double AsDouble() const;
-    const std::string& AsString() const;
-    const Array& AsArray() const;
-    const Dict& AsMap() const;
-    
-    bool operator==(const Node& other)const;
-    bool operator!=(const Node& other)const;
-    
-private:
-    Value value_;
+    Value& GetValue() {
+        return *this;
+    }    
 };
-    
+
+inline bool operator!=(const Node& lhs, const Node& rhs) {
+    return !(lhs == rhs);
+}
 
 class Document {
 public:
-    explicit Document(Node root);
-    const Node& GetRoot() const;
+    explicit Document(Node root)
+        : root_(std::move(root)) {
+    }
+
+    const Node& GetRoot() const {
+        return root_;
+    }
+
 private:
     Node root_;
 };
-    
-bool operator==(const Document& lhs, const Document& rhs);
-    
-std::ostream& operator<<(std::ostream& out, const Node& node); 
 
-Document Load(std::istream& input);  
+inline bool operator==(const Document& lhs, const Document& rhs) {
+    return lhs.GetRoot() == rhs.GetRoot();
+}
+
+inline bool operator!=(const Document& lhs, const Document& rhs) {
+    return !(lhs == rhs);
+}
+
+Document Load(std::istream& input);
 
 void Print(const Document& doc, std::ostream& output);
 
