ce7f5ef74bf6db66ea54c48f9238b5046c540f79
Ruslan5910 <150811455+Ruslan5910@users.noreply.github.com>
2025-02-13 14:59:39 +0300

    Update request_handler.cpp

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -1,27 +1,23 @@
-#include "request_handler.h"
+#include "request_handler.h" 
 
-RequestHandler::RequestHandler(const transport::TransportCatalogue& db, const MapRenderer& renderer, TransportRouter& transport_router)
-    : db_(db), renderer_(renderer), transport_router_(transport_router) {}
+RequestHandler::RequestHandler(const transport::TransportCatalogue& db, const MapRenderer& renderer, TransportRouter& transport_router) 
+    : db_(db), renderer_(renderer), transport_router_(transport_router) {} 
 
-std::optional<transport::TransportCatalogue::RouteInfo> RequestHandler::GetBusStat(const std::string_view& bus_name) const {
-    if (db_.GetRouteInfo(bus_name).count_stops == 0) {
-        return std::nullopt;
-    }
-    return db_.GetRouteInfo(bus_name);
-}
+std::optional<transport::TransportCatalogue::RouteInfo> RequestHandler::GetBusStat(const std::string_view& bus_name) const { 
+    if (db_.GetRouteInfo(bus_name).count_stops == 0) { 
+        return std::nullopt; 
+    } 
+    return db_.GetRouteInfo(bus_name); 
+} 
 
-const std::unordered_set<std::string_view>* RequestHandler::GetBusesByStop(const std::string_view& stop_name) const {
-    return db_.GetRoutesThrowStop(stop_name);
-}
+const std::unordered_set<std::string_view>* RequestHandler::GetBusesByStop(const std::string_view& stop_name) const { 
+    return db_.GetRoutesThrowStop(stop_name); 
+} 
 
-svg::Document RequestHandler::RenderMap() const {
-    return renderer_.MakeSvgDocument(db_.GetStops(), db_.GetRoutes());
-}
+svg::Document RequestHandler::RenderMap() const { 
+    return renderer_.MakeSvgDocument(db_.GetStops(), db_.GetRoutes()); 
+} 
 
-void RequestHandler::UploadTransportCatalogue() {
-    return transport_router_.AddGraph(db_);
-}
-
-std::optional<GraphResults> RequestHandler::BuildRoute(std::string_view from, std::string_view to) const {
-    return transport_router_.BuildRoute(from, to);
+std::optional<GraphResults> RequestHandler::BuildRoute(std::string_view from, std::string_view to) const { 
+    return transport_router_.BuildRoute(from, to); 
 }
